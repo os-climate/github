57cf2f51adb78def37a9f03ee6293be9c148ce04
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-12-11 16:53:13 +0000

    Chore: Fix script syntax
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- scripts/update-downstreams.sh
+++ scripts/update-downstreams.sh
@@ -9,15 +9,15 @@ REPOSITORY_LIST="scripts/repositories.txt"
 # Pre-flight checks
 GIT_CMD=$(which git)
 if [ ! -x "$GIT_CMD" ]; then
-	echo "Error: GIT command was not found in PATH"; exit 1
-elif [ ! -f "$REPOSITORY_LIST"]; then
-	echo "Error: missing repository list $REPOSITORY_LIST"; exit 1
+    echo "Error: GIT command was not found in PATH"; exit 1
+elif [ ! -f "$REPOSITORY_LIST" ]; then
+    echo "Error: missing repository list $REPOSITORY_LIST"; exit 1
 fi
 
 echo "Updating repositories with latest commit on main branch..."
 HASH=$(git log -n 1 main --pretty=format:"%H")
-
+echo "Last commit: $HASH"
 echo "Parsing repo list: $REPOSITORY_LIST"
-for REPO in $(cat "$REPOSITORY_LIST"); do
-	echo "Processing: $REPO"
+while IFS= read -r REPO < "$REPOSITORY_LIST"; do
+    echo "Processing: $REPO"
 done
